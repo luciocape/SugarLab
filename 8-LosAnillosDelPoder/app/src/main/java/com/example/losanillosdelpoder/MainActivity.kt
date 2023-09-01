5b1b73739079445e2620068a3ae3bf73fe199c12
LucioCapezzuto <luciodimascapezzuto@gmail.com>
2023-09-01 01:16:39 -0300

    Solución al problema en compu casa

--- 8-LosAnillosDelPoder/app/src/main/java/com/example/losanillosdelpoder/MainActivity.kt
+++ 8-LosAnillosDelPoder/app/src/main/java/com/example/losanillosdelpoder/MainActivity.kt
@@ -7,23 +7,46 @@ import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     //Derecha
-
-    val btnBullrich:ImageButton? = null
-    val btnLarreta:ImageButton? = null
-    val btnMarra:ImageButton? = null
-    val btnMacri:ImageButton? = null
+    var btnMilei:ImageButton? = null
+    var btnBullrich:ImageButton? = null
+    var btnLarreta:ImageButton? = null
+    var btnMarra:ImageButton? = null
+    var btnMacri:ImageButton? = null
+    val ataque:Map<String,Int> = mapOf(
+    "Cristina" to 7,
+    "Kirchner" to 0,
+    "Alberto" to -1,
+    "Massa" to (1..5).random(),
+    "Gravois" to 2,
+    "Milei" to 6,
+    "Macri" to 4,
+    "Bullrich" to 4,
+    "larreta" to 0,
+    "Marra" to 2
+    )
+    var guerreros:MutableList<Guerreros> = mutableListOf()
     //Izquierda
-    val btnMassa:ImageButton? = null
-    val btnAlberto:ImageButton? = null
-    val btnCristina:ImageButton? = null
-    val btnGrabois:ImageButton? = null
-    val btnKirchner:ImageButton? = null
+    /*
+    var btnMassa:ImageButton? = null
+    var btnAlberto:ImageButton? = null
+    var btnCristina:ImageButton? = null
+    var btnGrabois:ImageButton? = null
+    var btnKirchner:ImageButton? = null
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
         setContentView(R.layout.derecha)
-        setContentView(R.layout.izquierda)
-        setContentView(R.layout.lucha)
+
+
+        //Derecha
+        btnMilei = findViewById(R.id.btnMilei)
+        btnBullrich = findViewById(R.id.btnBullrich)
+        btnLarreta = findViewById(R.id.btnLarreta)
+        btnMarra = findViewById(R.id.btnMarra)
+        btnMacri = findViewById(R.id.btnMacri)
+
+        //Izquierda
         /*val ataque:Map<String,Int> = mapOf(
             "Cristina" to 7,
             "Kirchner" to 0,
@@ -48,20 +71,40 @@ class MainActivity : AppCompatActivity() {
             }
         }
         println(Pelea(guerreros))*/
-        var btnMilei: ImageButton = findViewById(R.id.btnMilei)
-        btnMilei.setOnClickListener(CallMilei())
+                //var btnMilei: ImageButton = findViewById(R.id.btnMilei)
+        //btnMilei.setOnClickListener(CallMilei())
 
     }
-    fun CallMilei(view:View){
-
-        btnBullrich = findViewById(R.id.btnBullrich)
-        btnLarreta = findViewById(R.id.btnLarreta)
-        btnMarra = findViewById(R.id.btnMarra)
-        btnMacri = findViewById(R.id.btnMacri)
-        kotlin.io.println(view.id.toString())
-        println("hola")
-        //guerreros.add(Guerreros(ataque.getValue("Milei"), true,(1..4).random()))
+    fun CallDerecha(view:View):MutableList<Guerreros>{
+        println(view.id.toString())
+        println("Derecha")
+        guerreros.add(Guerreros(ataque.getValue("Milei"), true,(1..4).random()))
     }
+    /*fun CallIzquierda(view: View){
+        btnMassa = findViewById(R.id.btnBullrich)
+        btnCristina = findViewById(R.id.btnLarreta)
+        btnAlberto = findViewById(R.id.btnMarra)
+        btnGrabois = findViewById(R.id.btnMacri)
+        btnKirchner = findViewById(R.id.btnBullrich)
+        println("Izquierda")
+    }
+     */
+
+    /*fun Candidatos(){
+
+        var indice:Int = 0
+
+        for (x in ataque.keys){
+            indice += 1
+            if (indice > 5){
+                guerreros.add(Guerreros(ataque.getValue(x), true,(1..4).random()))
+            }
+            else{
+                guerreros.add(Guerreros(ataque.getValue(x), false,(1..4).random()))
+            }
+        }
+    }
+     */
     fun Pelea(guerreros:MutableList<Guerreros>):String{
         var resultado:Int = 0
         var poderBuenos:Int = 0
