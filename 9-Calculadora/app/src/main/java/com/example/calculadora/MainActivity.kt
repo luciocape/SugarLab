6524d49fc7099a3bb9daec6bc3dfb2b76b26b05c
LucioCapezzuto <luciodimascapezzuto@gmail.com>
2023-09-11 20:05:52 -0300

    Calculadora funcionando perfectamente y con buen layout

--- 9-Calculadora/app/src/main/java/com/example/calculadora/MainActivity.kt
+++ 9-Calculadora/app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -2,103 +2,108 @@ package com.example.calculadora
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    var oprs:MutableList<Char> = mutableListOf()
-    var numeros:MutableList<Button> = mutableListOf()
-    var nums:MutableList<Double> = mutableListOf()
+
     val oprList:Array<Char> = arrayOf('-','+','x','/')
-    var btnIgual:Button ?= null
-    var btnMenos:Button ?= null
-    var btnMas:Button ?= null
-    var btnPor:Button ?= null
-    var btnDiv:Button ?= null
-    var btnReset:Button ?= null
+
+    var opr:Char? = null
+    var nums:MutableList<String> = mutableListOf("0.0")
+
+    var numero1:Boolean = false
+    var operando:Boolean = false
 
     var txtResultado:TextView ?= null
+    var btn1:Button ?= null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnIgual = findViewById(R.id.btnIgual)
-        btnMenos = findViewById(R.id.btnMenos)
-        btnMas = findViewById(R.id.btnMas)
-        btnDiv = findViewById(R.id.btnDiv)
-        btnPor = findViewById(R.id.btnX)
-        btnReset = findViewById(R.id.btnReset)
-
         txtResultado = findViewById(R.id.txtResultado)
-        Log.i("txtResultado",txtResultado?.text.toString())
-        if (nums.isEmpty()){
-                Log.i("numeros",nums.toString() )
-                Log.i("tamaño numeros",nums.size.toString())
-            }
+        btn1 = findViewById(R.id.btn1)
     }
-    fun Evaluar(nums:MutableList<Double>, oprs:MutableList<Char>):String{
-        var resultado:Double = nums[0]
-        if (nums.size >= 2){
-            for (x in oprs.indices){
-                when (oprs[x]){
-                    '+' -> resultado += nums[x+1]
-                    '-' -> resultado -= nums[x+1]
-                    'x' -> resultado *= nums[x+1]
-                    '/' -> resultado /= nums[x+1]
-                }
-            }
-        }
-        else if (nums.isNotEmpty()){
-            when (oprs[0]){
-                '+' -> resultado = nums[0] + nums[0]
-                '-' -> nums[0] - nums[0]
-                'x' -> nums[0] * nums[0]
-                '/' -> nums[0] / nums[0]
-            }
-        }
-        else return "0.0"
-        return resultado.toString()
-
-    }
-    fun Calcular(view: View){
-        val boton = view as Button
-        val textoBoton:String = boton.text.toString()
-        var mostrar = txtResultado?.text.toString()
-        if ( textoBoton.toCharArray()[0] in oprList){
-            if (nums.isEmpty()){
-                nums.add(txtResultado?.text.toString().toDouble())
-                oprs.add(textoBoton.toCharArray()[0])
-            }
-            else if (nums.size > oprs.size){
-                oprs.add(textoBoton.toCharArray()[0])
-                nums.add(txtResultado?.text.toString().toDouble())
-            }
-            else{
-                oprs[oprs.lastIndex] = textoBoton.toCharArray()[0]
-            }
-            txtResultado?.text = oprs.last().toString()
-        }
-        else if (textoBoton != "Reset" && textoBoton != "="){
-            val concatenar = mostrar + textoBoton
-            mostrar = Limpieza(concatenar)
-            txtResultado?.text  = mostrar
-        }
-        if (textoBoton == "="){
-            txtResultado?.text  = Evaluar(nums, oprs)
-        }
-        else if (textoBoton == "Reset") txtResultado?.text = "0.0"
-    }
-    fun Limpieza(str:String):String{
+    //Agregar números y limpiar ceros, puntos, operadores, etc. de atras
+    fun limpieza(str:String):String{
         var i = 0
         val sb = StringBuffer(str)
-        while (i < str.length && (str[i] == '0' || str[i] == '.' || str[i] in oprs)) {
+        while (i < str.length && (str[i] == '0' || str[i] == '.' || str[i] in oprList)) {
             i++
             sb.replace(0,i,"")
         }
         return sb.toString()
     }
+    fun agregarNumero(view: View){
+        val boton = view as Button
+        val textoBoton = boton.text.toString()
+        //Si el hay un operador agregado se puede añadir un segundo número
+        if  (operando){
+            if (nums.size < 2){
+                nums.add(textoBoton)
+            }
+            else {
+                nums[1]+=textoBoton
+            }
+            nums[1]=limpieza(nums[1])
+            val resultado = txtResultado?.text.toString() + nums[1]
+            txtResultado?.text = resultado
+        }
+        else if(!numero1){
+            nums[0]+=boton.text.toString()
+            nums[0]=limpieza(nums[0])
+            txtResultado?.text = nums[0]
+        }
+    }
+    //Operación a realizar
+    fun agregarOperador(view: View){
+        val boton = view as Button
+        val textoBoton = boton.text.toString()
+        //Si todavía no hay un segundo número agregado
+        if (nums.size == 1){
+            opr = textoBoton.toCharArray()[0]
+            operando = true
+            val resultado = txtResultado?.text.toString() + opr.toString()
+            txtResultado?.text = resultado
+        }
+    }
+    //Evaluar expresión
+    fun evaluar(view: View){
+        var num1 = nums[0].toDouble()
+        val num2 = nums[1].toDouble()
+        when (opr){
+            '+' -> num1 += num2
+            '-' -> num1 -= num2
+            'x' -> num1 *= num2
+            '/' -> num1 /= num2
+        }
+        reset(view)
+        nums.add(num1.toString())
+    }
+    //Devolver resultado
+    fun igualar(view: View){
+        if (opr != null){
+            evaluar(view)
+            txtResultado?.text = nums[0]
+            numero1=true
+        }
+
+    }
+    //Resetear calculadora
+    fun reset(view: View){
+        val boton = view as Button
+        val textoBoton = boton.text.toString()
+        nums.removeIf{it in nums}
+        if (textoBoton == "Reset"){
+            txtResultado?.text = "0.0"
+            nums.add("0.0")
+            numero1 = false
+        }
+        operando = false
+        opr = null
+    }
+
 }
